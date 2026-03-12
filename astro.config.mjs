7b456d2575960b5d7fdc3cc88234bef929c687c2
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2026-03-12 09:38:18 +0100

    arreglar temas de loading desde /id/*

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 export default defineConfig({
   site: "https://aedm.org.es",
   output: "static",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
