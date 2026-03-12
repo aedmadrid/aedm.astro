a683f69b2fb7786227295393336089161aaaa5ca
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2026-03-12 09:41:33 +0100

    arreglar 404 en producción

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 export default defineConfig({
   site: "https://aedm.org.es",
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
