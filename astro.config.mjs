707e9fe22184ecf5f0291cacaad88f9010ccde3b
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2026-04-13 22:33:38 +0200

    cositas varias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://aedm.org.es",
   output: "static",
+  adapter: cloudflare()
 });
