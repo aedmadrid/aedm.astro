83a50d901dca456a3ca15e8f7c12a66f41670c1b
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2026-06-29 23:03:23 +0200

    sitemap google search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://aedm.org.es",
   output: "static",
   adapter: cloudflare(),
+  integrations: [sitemap()],
   redirects: {},
 });
